42fc16dca81115cb226676dcb65e75af2de1283c
narek <narekb91@gmail.com>
2025-04-06 15:04:58 +0400

    Deploy Flutter web app to GitHub Pages

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ class GameScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.yellow,
       body: Center(
         child: Text('Game will go here'),
       ),
