ff597fec12f2cb367f26ce38d694565c277b75a3
narek <narekb91@gmail.com>
2025-04-06 14:49:01 +0400

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyGameApp());
+}
+
+class MyGameApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Web Game',
+      debugShowCheckedModeBanner: false,
+      home: GameScreen(),
+    );
+  }
+}
+
+class GameScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Game will go here'),
+      ),
+    );
+  }
+}
